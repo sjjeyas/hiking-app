14b8f0cabf526fe101bad3e86ef49325a8771206
Nicole Concepcion <nconcepc@usc.edu>
2024-11-03 23:24:52 -0800

    new frontend for login/signup

--- Project/app/src/main/java/com/example/project/MainActivity.java
+++ Project/app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.project.classes.User;
 
 public class MainActivity extends AppCompatActivity {
+    Intent intent = getIntent();
+    String UID = intent.getStringExtra("UID");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
